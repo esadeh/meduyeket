02998dcbe37ed158413520b43a327007c4d6a980
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2022-01-22 23:49:06 +0200

    fix gerund handling, and improve code of generating scripts

--- word_lists/generate_words.py
+++ word_lists/generate_words.py
@@ -1,26 +1,25 @@
 import csv
 import random
 
-words = set()
-words.update(line.strip() for line in open('hspell-nouns.txt'))
-words.update(line.strip() for line in open('hspell-shemp.txt') if not line.strip().endswith('ת'))
-words.update(line.strip() for line in open('hspell-other.txt'))
-words.update(line.strip() for line in open('hspell-easy-verbs.txt'))
-all_words = set(words)
-all_words.update(line.strip() for line in open('hspell-shemp.txt'))
+easy_words, all_words = set(), set()
+
+all_words.update(line.strip() for line in open('hspell-nouns.txt'))
 all_words.update(line.strip() for line in open('hspell-verbs.txt'))
+all_words.update(line.strip() for line in open('hspell-other.txt'))
+
+easy_words.update(line.strip() for line in open('hspell-easy-nouns.txt'))
+easy_words.update(line.strip() for line in open('hspell-easy-verbs.txt'))
+easy_words.update(line.strip() for line in open('hspell-other.txt'))
 
 freqs = {w: int(c) for w, c in csv.reader(open('cc100.csv')) if len(w) == 5}
 
 past_meduyakot = ['כימאי', 'מעטפת', 'נקיות', 'חיסון', 'שתיקה', 'מבואר', 'יתגלה', 'רשאים', 'הוביל']
 missing_words = ['כורסא', 'ארצני' ,'משטור', 'עקידה', 'כאילו', 'האמנם']
 
-words.update(past_meduyakot)
-words.update(missing_words)
-all_words.update(past_meduyakot)
-all_words.update(missing_words)
+all_words.update(missing_words + past_meduyakot)
+easy_words.update(past_meduyakot)
 
-meduyakot = [w for w in words if freqs.get(w, 0) >= 20000 and w not in past_meduyakot]
+meduyakot = [w for w in easy_words if freqs.get(w, 0) >= 20000 and w not in past_meduyakot]
 random.shuffle(meduyakot)
 meduyakot = meduyakot[:14] + past_meduyakot + meduyakot[14:]
 
--- word_lists/hspell_generation.sh
+++ word_lists/hspell_generation.sh
@@ -1,15 +1,30 @@
 # The hspell-*.txt files are based on the Hspell 1.4 distribution. Here are the
 # commands used to generate them:
 
-perl -w wolig.pl -d wolig.dat | iconv -f windows-1255 | grep -v '^#' | egrep ',(יחיד|רבים)' | cut -d, -f1 | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 > /meduyeket/word_lists/hspell-nouns.txt
-for f in extrawords.hif milot.hif biza-nouns.hif; do iconv -f windows-1255 $f | egrep '^[א-תךםןףץ]{5}$' >> /meduyeket/word_lists/hspell-other.txt; iconv -f windows-1255 $f | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-other.txt; done
+perl -w woo -d woo.dat | sed "/\+/d" > verbs.hif
+perl -w wolig.pl -d shemp.dat > shemp.hif
+perl -w wolig.pl -d wolig.dat > nouns.hif
 
-perl -w woo -d woo.dat | iconv -f windows-1255 | grep -v '^#' | grep -v ',כינוי' | grep -v ',סמיכות' | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 > /meduyeket/word_lists/hspell-verbs.txt
-perl -w woo -d woo.dat | iconv -f windows-1255 | grep -v '^#' | grep -v ',כינוי' | grep -v ',סמיכות' | grep -v ',ציווי' | grep -v ',את' | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 > /meduyeket/word_lists/hspell-easy-verbs.txt
-iconv -f windows-1255 biza-verbs.hif | grep -v '^#' | grep -v ',כינוי' | grep -v ',סמיכות' | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-verbs.txt
-iconv -f windows-1255 biza-verbs.hif | grep -v '^#' | grep -v ',כינוי' | grep -v ',סמיכות' | grep -v ',ציווי' | grep -v ',את' | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-easy-verbs.txt
+rm /meduyeket/word_lists/hspell-*.txt
 
-perl -w wolig.pl -d shemp.dat | iconv -f windows-1255 | grep -v '^#' | egrep ',(יחיד|רבים)' | cut -d, -f1 | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 > /meduyeket/word_lists/hspell-shemp.txt
+for f in nouns.hif shemp.hif
+  do
+    iconv -f windows-1255 $f | grep -v '^#' | grep -v ',סמיכות' | cut -d, -f1 | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-nouns.txt
+    iconv -f windows-1255 $f | grep -v '^#' | grep -v ',סמיכות' | grep -v ',של/' | cut -d, -f1 | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-easy-nouns.txt
+done
+for f in extrawords.hif milot.hif biza-nouns.hif
+  do
+    iconv -f windows-1255 $f | egrep '^[א-תךםןףץ]{5}$' >> /meduyeket/word_lists/hspell-other.txt
+    iconv -f windows-1255 $f | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-other.txt
+done
+
+for f in verbs.hif biza-verbs.hif
+  do
+    iconv -f windows-1255 $f | grep -v '^#' | grep -v ',סמיכות' | grep -v ',כינוי/' | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-verbs.txt
+    iconv -f windows-1255 $f | grep -v '^#' | grep -v ',סמיכות' | grep -v ',כינוי/' | grep -v ',ציווי' | grep -v ',את' | egrep '^[א-תךםןףץ]{5} ' | cut -d' ' -f1 >> /meduyeket/word_lists/hspell-easy-verbs.txt
+done
+
+wc -l /meduyeket/word_lists/hspell-*.txt
 
 # The corpus word count cc100.csv is downloaded from
 #   https://github.com/eyaler/hebrew_wordlists
